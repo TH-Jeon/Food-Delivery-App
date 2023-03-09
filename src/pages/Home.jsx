e1728dc4d322c630f0f4515cd8ef0acc16d6dce1
TH-Jeon <89818381+TH-Jeon@users.noreply.github.com>
2023-03-08 21:38:17 -0500

    Header & Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Helmet from '../components/Helmet/Helmet.js';
+import { Container, Row, Col } from 'reactstrap';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <Helmet title=' Home'></Helmet>;
 };
 
 export default Home;
