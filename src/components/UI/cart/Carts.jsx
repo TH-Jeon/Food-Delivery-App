5d0994d6dc3b2f95b360e30b3cb3e7076cf212ee
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2023-03-13 11:10:32 -0400

    cart CRUD

--- src/components/UI/cart/Carts.jsx
+++ src/components/UI/cart/Carts.jsx
@@ -3,40 +3,57 @@ import React from 'react';
 import { ListGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import CartItem from './CartItem';
+import { useDispatch , useSelector} from 'react-redux';
+import { cartUiActions } from '../../../store/shopping-cart/cartUiSlice';
 
-import '../../../styles/shopping-cart.css'
+import '../../../styles/shopping-cart.css';
 
 const Carts = () => {
-   return(
 
-       <div className="cart__container">
+    const dispatch = useDispatch()
+    const cartProducts = useSelector(state=>state.cart.cartItems)
+
+    const toggleCart=()=>{
+        dispatch(cartUiActions.toggle())
+    }
+
+   const totalAmount = useSelector(state =>state.cart.totalAmount)
+
+    
+   return (
+      <div className="cart__container">
       <ListGroup className="cart">
-         <div className="cart__close">
-            <span>
-               <i class="ri-close-fill"></i>
-            </span>
-         </div>
-
-         <div className="cart__item-list">
-            <CartItem />
-       
-         </div>
-<div>
-
-         <span className="delete__btn" >
-            <i class="ri-close-line"></i>
+        <div className="cart__close">
+          <span onClick={toggleCart}>
+            <i class="ri-close-fill"></i>
           </span>
-</div>
+        </div>
+            <div className="cart__item-list">
 
-         <div className="cart__bottom d-flex align-items-center justify-content-between">
-            <h6>
-               amount : <span>$123</span>
-            </h6>
-            <button><Link to='/checkout'>Check out</Link></button>
-         </div>
-      </ListGroup>
-   </div>
-       )
+               {
+                  cartProducts.length === 0 ? <h6 className='text-center mt-5'>No Item added to the cart</h6> : cartProducts.map((item,index)=>(
+                     <CartItem item={item} key={index}/>
+                  )) 
+               }
+             
+            </div>
+            <div>
+               <span className="delete__btn">
+                  <i class="ri-close-line"></i>
+               </span>
+            </div>
+
+            <div className="cart__bottom d-flex align-items-center justify-content-between">
+               <h6>
+                  amount : <span>${totalAmount}</span>
+               </h6>
+               <button>
+                  <Link to="/checkout">Check out</Link>
+               </button>
+            </div>
+         </ListGroup>
+      </div>
+   );
 };
 
 export default Carts;
