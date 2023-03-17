8daf7182d3291a5be25d9c871fafd480bb9f0541
TH-Jeon <89818381+TH-Jeon@users.noreply.github.com>
2023-03-16 20:56:10 -0400

    Search, Sorting & Page # in All Foods

--- src/components/UI/cart/CartItem.jsx
+++ src/components/UI/cart/CartItem.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
 import { ListGroupItem } from 'reactstrap';
-import productImg from '../../../assets/images/product_01.1.jpg';
-import '../../../styles/cart-item.css';
+
+import "../../../styles/cart-item.css";
 
 import { useDispatch } from 'react-redux';
-import {cartActions} from '../../../store/shopping-cart/cartSlice'
+import { cartActions } from '../../../store/shopping-cart/cartSlice'
 
-const CartItem = ({item}) => {
 
-   const {id,title,price,image01,quantity,totalPrice} =item 
+const CartItem = ({ item }) => {
 
-   const dispatch=useDispatch();
+   const { id, title, price, image01, quantity, totalPrice } = item;
 
-   const incrementItem = ()=>{
+   const dispatch = useDispatch();
+
+   const incrementItem = () => {
       dispatch(cartActions.addItem({
          id,
          title,
          price,
-         image01
-      }))
-   }
+         image01,
+         })
+      );
+   };
 
-   const decreaseItem= ()=>{
-      dispatch(cartActions.removeItem(id))
-   }
+   const decreaseItem = () => {
+      dispatch(cartActions.removeItem(id));
+   };
 
-   const deleteItem =( ) => {
-dispatch(cartActions.deleteItem(id))
-   }
+   const deleteItem = () => {
+      dispatch(cartActions.deleteItem(id));
+   };
 
    return (
-      <ListGroupItem className="border-0  cart__item" >
+      <ListGroupItem className="border-0 cart__item">
          <div className="cart__item-info d-flex gap-2">
-            <img src={image01} alt="productImg" />
+            <img src={image01} alt="product-img" />
 
-            <div className="art__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
+            <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
                <div>
-                  <h6 className="cart-product-title">{title}</h6>
-                  <p className=" d-flex align-items-center gap-5 cart__product-price">
-                     {quantity}X <span>${totalPrice}</span>
+                  <h6 className="cart-product-title">{ title }</h6>
+                  <p className="d-flex align-items-center gap-5 cart__product-price">
+                     { quantity }X <span>${ totalPrice }</span>
                   </p>
-                  <div className='d-flex align-items-center gap-3 increase__decrease-btn'>
+                  <div className='d-flex align-items-center justify-content-between increase__decrease-btn'>
                      <span className='increase__btn' onClick={incrementItem}>
                         <i class="ri-add-line"></i>
                      </span>
@@ -51,9 +53,9 @@ dispatch(cartActions.deleteItem(id))
                   </div>
                </div>
 
-               <span className="delete__btn" onClick={deleteItem} >
-            <i class="ri-close-line"></i>
-          </span>
+               <span className="delete__btn" onClick={deleteItem}>
+                  <i class="ri-close-line"></i>
+               </span>
             </div>
          </div>
       </ListGroupItem>
--- src/components/UI/cart/Carts.jsx
+++ src/components/UI/cart/Carts.jsx
@@ -10,45 +10,40 @@ import '../../../styles/shopping-cart.css';
 
 const Carts = () => {
 
-    const dispatch = useDispatch()
-    const cartProducts = useSelector(state=>state.cart.cartItems)
+    const dispatch = useDispatch();
+    const cartProducts = useSelector((state) => state.cart.cartItems);
+    const totalAmount = useSelector((state) => state.cart.totalAmount);
 
     const toggleCart=()=>{
-        dispatch(cartUiActions.toggle())
-    }
-
-   const totalAmount = useSelector(state =>state.cart.totalAmount)
-
+        dispatch(cartUiActions.toggle());
+    };
     
    return (
       <div className="cart__container">
-      <ListGroup className="cart">
-        <div className="cart__close">
-          <span onClick={toggleCart}>
-            <i class="ri-close-fill"></i>
-          </span>
-        </div>
-            <div className="cart__item-list">
-
-               {
-                  cartProducts.length === 0 ? <h6 className='text-center mt-5'>No Item added to the cart</h6> : cartProducts.map((item,index)=>(
-                     <CartItem item={item} key={index}/>
-                  )) 
-               }
-             
-            </div>
-            <div>
-               <span className="delete__btn">
-                  <i class="ri-close-line"></i>
+         <ListGroup className="cart">
+            <div className="cart__close">
+               <span onClick={toggleCart}>
+                  <i class="ri-close-fill"></i>
                </span>
+            </div>
+
+            <div className="cart__item-list">
+               {
+                  cartProducts.length === 0 ? (
+                     <h6 className='text-center mt-5'>No item added to the cart</h6> 
+                  ) : (
+                     cartProducts.map((item, index) => (
+                        <CartItem item={item} key={index} />   
+                     ))
+                  )}
             </div>
 
             <div className="cart__bottom d-flex align-items-center justify-content-between">
                <h6>
-                  amount : <span>${totalAmount}</span>
+                  Subtotal : <span>${totalAmount}</span>
                </h6>
                <button>
-                  <Link to="/checkout">Check out</Link>
+                  <Link to="/checkout" onClick={toggleCart}>Checkout</Link>
                </button>
             </div>
          </ListGroup>
