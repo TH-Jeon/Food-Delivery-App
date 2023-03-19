d948520aa8e2c173c7e8146c2e25b0a30c9a1720
TH-Jeon <89818381+TH-Jeon@users.noreply.github.com>
2023-03-19 01:40:27 -0400

    Food Details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,66 +1,62 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
-import { Container } from 'reactstrap';
-import logo from '../../assets/images/res-logo.png';
-import { NavLink, Link } from 'react-router-dom';
-import { useSelector,useDispatch } from 'react-redux'; 
+import { Container } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+import { NavLink, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { cartUiActions } from '../../store/shopping-cart/cartUiSlice';
+import { cartUiActions } from "../../store/shopping-cart/cartUiSlice";
 
-import '../../styles/header.css';
+import "../../styles/header.css";
 
 const nav__links = [
   {
-    display: 'Home',
-    path: '/home',
+    display: "Home",
+    path: "/home",
   },
   {
-    display: 'Foods',
-    path: '/foods',
+    display: "Foods",
+    path: "/foods",
   },
   {
-    display: 'Cart',
-    path: '/cart',
+    display: "Cart",
+    path: "/cart",
   },
   {
-    display: 'Contact',
-    path: '/contact',
+    display: "Contact",
+    path: "/contact",
   },
 ];
 
 const Header = () => {
-
   const menuRef = useRef(null);
   const headerRef = useRef(null);
-  const totalQuantity= useSelector((state) => state.cart.totalQuantity);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
   const dispatch = useDispatch();
 
-  const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
 
   const toggleCart = () => {
     dispatch(cartUiActions.toggle());
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) 
-      {
-        headerRef.current.classList.add('header__shrink');
-      }
-
-      else 
-      {
-        headerRef.current.classList.remove('header__shrink');
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("header__shrink");
+      } else {
+        headerRef.current.classList.remove("header__shrink");
       }
     });
-    
-    return () => window.removeEventListener('scroll');
-    
+
+    return () => window.removeEventListener("scroll");
   }, []);
 
   return (
-    <header className='header' ref={headerRef}>
-
+    <header className="header" ref={headerRef}>
       <Container>
         <div className="nav__wrapper d-flex align-items-center justify-content-between">
           <div className="logo">
@@ -73,20 +69,19 @@ const Header = () => {
             <div className="menu d-flex align-items-center gap-5">
               {nav__links.map((item, index) => (
                 <NavLink
-                  
-                  to={item.path} 
+                  to={item.path}
                   key={index}
-                  className={(navClass) => 
-                  navClass.isActive ? "active__menu" : ""
+                  className={(navClass) =>
+                    navClass.isActive ? "active__menu" : ""
                   }
                 >
                   {item.display}
                 </NavLink>
-                ))}
+              ))}
             </div>
           </div>
 
-          {/* ======= nav right icons ======= */}
+          {/* ======== nav right icons ========= */}
           <div className="nav__right d-flex align-items-center gap-4">
             <span className="cart__icon" onClick={toggleCart}>
               <i class="ri-shopping-basket-line"></i>
@@ -94,7 +89,8 @@ const Header = () => {
             </span>
 
             <span className="user">
-              <Link to='/login'><i class="ri-user-line"></i>
+              <Link to="/login">
+                <i class="ri-user-line"></i>
               </Link>
             </span>
 
--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,58 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import Helmet from '../components/Helmet/Helmet';
-import CommonSection from '../components/UI/common-section/CommonSection';
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 
-import products from '../assets/fake-data/products';
-import ProductCard from '../components/UI/product-card/ProductCard';
-import ReactPaginate from 'react-paginate';
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
+import ReactPaginate from "react-paginate";
 
-import '../styles/all-foods.css';
-import '../styles/pagination.css';
-
+import "../styles/all-foods.css";
+import "../styles/pagination.css";
 
 const AllFoods = () => {
-
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const [pageNumber, setPageNumber] = useState(0);
 
-  const searchedProduct = products.filter(( item ) => {
-    if (searchTerm.value === '') return item;
-    if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
-
-    return item;
-})
+  const searchedProduct = products.filter((item) => {
+    if (searchTerm.value === "") {
+      return item;
+    }
+    if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return item;
+    } else {
+      return console.log("not found");
+    }
+  });
 
   const productPerPage = 12;
   const visitedPage = pageNumber * productPerPage;
-  const displayPage = searchedProduct.slice(visitedPage, visitedPage + productPerPage);
-  
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+
   const pageCount = Math.ceil(searchedProduct.length / productPerPage);
 
-  const changePage = ({selected}) => {
+  const changePage = ({ selected }) => {
     setPageNumber(selected);
-  }
+  };
 
   return (
-    <Helmet title='All-Foods'>
-      <CommonSection title='All Foods' />
+    <Helmet title="All-Foods">
+      <CommonSection title="All Foods" />
 
       <section>
         <Container>
           <Row>
-            <Col lg='6' md='6' sm='6'>
-              <div className='search__widget d-flex align-items-center justify-content-between w-50'>
-                <input type="text" placeholder="I'm looking for ...." value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+            <Col lg="6" md="6" sm="6" xs="12">
+              <div className="search__widget d-flex align-items-center justify-content-between ">
+                <input
+                  type="text"
+                  placeholder="I'm looking for...."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <span>
                   <i class="ri-search-line"></i>
                 </span>
               </div>
             </Col>
-            <Col lg='6' md='6' sm='6' className='mb-5'>
-              <div className='sorting__widget text-end'>
-                <select className='w-50'>
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
                   <option>Default</option>
                   <option value="ascending">Alphabetically, A-Z</option>
                   <option value="descending">Alphabetically, Z-A</option>
@@ -62,24 +72,21 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            {
-              displayPage
-            .map(( item ) => (
-              <Col lg='3' md='4' sm='6' xs='6' key={ item.id } className='mb-4'>
-                <ProductCard item={ item }  />
+            {displayPage.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                <ProductCard item={item} />
               </Col>
-              ))}
+            ))}
 
-              <div>
-                <ReactPaginate
-                  pageCount = { pageCount }
-                  onPageChange = { changePage }
-                  previousLabel = {'Prev'}
-                  nextLabel = {'Next'}
-                  containerClassName = 'paginationBttns'
-                />
-              </div>
-
+            <div>
+              <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={changePage}
+                previousLabel={"Prev"}
+                nextLabel={"Next"}
+                containerClassName=" paginationBttns "
+              />
+            </div>
           </Row>
         </Container>
       </section>
